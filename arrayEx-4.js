bd6e4acb6749377ae8291c15de25063b9c3923d1
sidro <slacky2005@gmail.com>
2022-03-10 20:31:21 +0200

    code stylization

--- arrayEx-4.js
+++ arrayEx-4.js
@@ -1,7 +1,9 @@
 var ourArray = [1, 2, 3];
 var removedFromOurArray = ourArray.pop(); // remove last element from ourArray
-var myArray = [["John", 23], ["cat", 2]];
+var myArray = [
+  ["John", 23],
+  ["cat", 2],
+];
 var removedFromMyArray = myArray.pop(); // remove last element from myArray
 console.log(ourArray);
 console.log(myArray);
-
